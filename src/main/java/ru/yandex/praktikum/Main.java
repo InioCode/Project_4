5e67885ea7f836b1e34ea519f46f1cacd6d4928d
Nikolai Kudryashov <kolxx199@ya.ru>
2024-02-25 23:20:56 +0400

    add test for order and FAQ

---
+++ src/main/java/ru/yandex/praktikum/Main.java
@@ -0,0 +1,12 @@
+package ru.yandex.praktikum;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import ru.yandex.praktikum.pageObject.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
